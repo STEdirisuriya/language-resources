6395e340b87a591a9e5d2593ef23391ef4a200f5
Pasindu De Silva <pasindu@google.com>
2018-08-21 21:28:49 +0800

    Simeple shell to copy

---
+++ simpleaddress/copy.sh
@@ -0,0 +1,30 @@
+
+
+
+
+chmod 755 simpleaddress/mini/bazel_simpleaddress.far
+cp simpleaddress/mini/bazel_simpleaddress.far \
+/google/data/rw/teams/nemo-team/grmfar2/bazel_mini.far
+
+cp simpleaddress/mini/simpleaddress.far \
+/google/data/rw/teams/nemo-team/grmfar2/pure_thrax_mini.far
+
+
+chmod 755 simpleaddress/address_local_cleanspaces/bazel_address.far
+cp simpleaddress/address_local_cleanspaces/bazel_address.far \
+/google/data/rw/teams/nemo-team/grmfar2/bazel_address_local_cleanspaces.far
+
+
+cp simpleaddress/address_local_cleanspaces/address.far \
+/google/data/rw/teams/nemo-team/grmfar2/pure_thrax_address_local_cleanspaces.far
+
+
+
+chmod 755 simpleaddress/address_with_universal_cleanspaces/bazel_address.far
+cp simpleaddress/address_with_universal_cleanspaces/bazel_address.far \
+/google/data/rw/teams/nemo-team/grmfar2/bazel_address_with_universal_cleanspaces.far
+
+
+cp simpleaddress/address_with_universal_cleanspaces/address.far \
+/google/data/rw/teams/nemo-team/grmfar2/pure_thrax_address_with_universal_cleanspaces.far
+
